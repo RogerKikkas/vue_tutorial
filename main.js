e76db5ea9b2f129ec8e078c5974c3f7df52b9934
RKikkas <roger.kikkas@gmail.com>
2019-04-22 21:06:19 +0300

    Episode 13

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+window.Event = new class {
+    constructor() {
+        this.vue = new Vue();
+    }
+
+    fire(event, data = null) {
+        this.vue.$emit(event, data);
+    }
+
+    listen(event, callback) {
+        this.vue.$on(event, callback);
+    }
+};
+
 Vue.component('coupon', {
    template: `
         <input placeholder="Enter your coupon code" @blur="onCouponApplied">
@@ -5,7 +19,7 @@ Vue.component('coupon', {
 
     methods: {
        onCouponApplied() {
-           this.$emit('applied')
+           Event.fire('applied')
        }
     }
 });
@@ -16,10 +30,8 @@ var app = new Vue({
         couponApplied: false
     },
 
-    methods: {
-        onCouponApplied() {
-            this.couponApplied = true;
-        }
+    created() {
+        Event.listen('applied', () => alert('Handling it'))
     }
 });
 
