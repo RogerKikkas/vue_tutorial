8c2215fb2a11bec5221eba278c2eba215c1a25c4
RKikkas <roger.kikkas@gmail.com>
2019-04-23 17:35:45 +0300

    Episode 14

--- main.js
+++ main.js
@@ -1,27 +1,30 @@
-window.Event = new class {
-    constructor() {
-        this.vue = new Vue();
-    }
-
-    fire(event, data = null) {
-        this.vue.$emit(event, data);
-    }
-
-    listen(event, callback) {
-        this.vue.$on(event, callback);
-    }
-};
-
-Vue.component('coupon', {
-   template: `
-        <input placeholder="Enter your coupon code" @blur="onCouponApplied">
-   `,
-
-    methods: {
-       onCouponApplied() {
-           Event.fire('applied')
-       }
-    }
+Vue.component('modal', {
+    template: `
+        <div class="modal is-active">
+          <div class="modal-background"></div>
+          
+          <div class="modal-card">
+            <header class="modal-card-head">
+              <p class="modal-card-title">
+                <slot name="header"></slot>
+              </p>
+              <button class="delete" aria-label="close"></button>
+            </header>
+            
+            <section class="modal-card-body">
+                <slot>
+                    Default content here.
+                </slot>
+            </section>
+            
+            <footer class="modal-card-foot">
+              <slot name="footer">
+                <a class="button is-primary">Okay</a>
+              </slot>
+            </footer>
+          </div>
+        </div>
+    `
 });
 
 var app = new Vue({
@@ -181,6 +184,36 @@ Vue.component('tab', {
 
     mounted() {
         this.isActive = this.selected;
+    }
+});
+*/
+
+// COUPON
+
+/*
+window.Event = new class {
+    constructor() {
+        this.vue = new Vue();
+    }
+
+    fire(event, data = null) {
+        this.vue.$emit(event, data);
+    }
+
+    listen(event, callback) {
+        this.vue.$on(event, callback);
+    }
+};
+
+Vue.component('coupon', {
+   template: `
+        <input placeholder="Enter your coupon code" @blur="onCouponApplied">
+   `,
+
+    methods: {
+       onCouponApplied() {
+           Event.fire('applied')
+       }
     }
 });
 */
