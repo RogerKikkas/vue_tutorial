524267b0b8a9e0683f521b6f4503d35c93865531
RKikkas <roger.kikkas@gmail.com>
2019-04-24 21:19:32 +0300

    Episode 19

--- vue-app/public/js/app.js
+++ vue-app/public/js/app.js
@@ -1,3 +1,51 @@
+class Errors {
+    constructor() {
+        this.errors = {};
+    }
+
+    get(field) {
+        if (this.errors.errors) {
+            return this.errors.errors[field];
+        }
+    }
+
+    record(errors) {
+        this.errors = errors;
+    }
+
+    clear(field) {
+        if(this.errors.errors.field) {
+            delete this.errors.errors.field;
+        }
+    }
+}
+
+const app = new Vue({
+    el: '#app',
+
+    data: {
+        name: '',
+        description: '',
+        errors: new Errors()
+    },
+
+    methods: {
+        onSubmit() {
+            axios.post('/projects', this.$data)
+                .then(this.onSuccess)
+                .catch(error => this.errors.record(error.response.data));
+        },
+
+        onSuccess(response) {
+            alert(response.data.message);
+
+            this.name = '';
+            this.description = '';
+        }
+    },
+});
+
+/*
 new Vue({
     el: '#app',
 
@@ -11,3 +59,4 @@ new Vue({
         axios.get('/skills').then(response => this.skills = response.data);
     }
 });
+*/
